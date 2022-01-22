39dde40a5305e929001164f5bb6e26b8db4f84fc
biork23 <ismaelbiork@gmail.com>
2022-01-21 20:55:42 -0600

    codigo arreglado

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
+
 const params = require('yargs-parser')(process.argv.slice(2))
 const colors = require('colors')
-console.log('params:' , params)
 
-const nombres = [
-    'carlos',
-    'sara',
-    'Erik',
-    'Ivonne',
-    'Alberto',
-    'Leo',
-    'Alex',
-    'Airy',
-    'freddy',
-    'Jose',
-    'carlo'
-  ]
-
-  const koderName = params.name
-  const exist = nombres.includes(koderName)
-
-  if (exist) {
-      console.log('Hola koders'.rainbow)
-  } else {
-      console.log('Quien eres'.rainbow)
-  }
+const nombre = params.name
+console.log(`hola koder ${nombre}`.rainbow)
 
 /**
  * Un script que reciba mi nombre como parametro y que nos salude con colores de arcoiris
