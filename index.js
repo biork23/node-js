f5251280042d933a9e2dae55bc46ace7eb8cbc7c
biork23 <ismaelbiork@gmail.com>
2022-01-24 19:04:30 -0600

    fs.readfile_exercice

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 
-const params = require('yargs-parser')(process.argv.slice(2))
-const colors = require('colors')
+// const params = require('yargs-parser')(process.argv.slice(2))
+// const colors = require('colors')
 
-const nombre = params.name
-console.log(`hola koder ${nombre}`.rainbow)
+// const nombre = params.name
+// console.log(`hola koder ${nombre}`.rainbow)
 
-/**
- * Un script que reciba mi nombre como parametro y que nos salude con colores de arcoiris
- * node saludo.js --nombre=charles
- * - Debe ser un nuevo proyecto de node
- * - A terminar compartir el repositorio de github 
- *  - Que no este versionada la caarpeta node_modules
- *  - Que corra el proyecto
- */
+// /**
+//  * Un script que reciba mi nombre como parametro y que nos salude con colores de arcoiris
+//  * node saludo.js --nombre=charles
+//  * - Debe ser un nuevo proyecto de node
+//  * - A terminar compartir el repositorio de github 
+//  *  - Que no este versionada la caarpeta node_modules
+//  *  - Que corra el proyecto
+//  */
+
+const fs = require('fs')
+
+fs.readFile('text.txt', 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log(data)
+})
